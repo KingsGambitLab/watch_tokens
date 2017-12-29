6672826f73546dc48b835035df2fc74f736e8b25
rachin <rachin@pepo.com>
2017-12-30 05:28:53 +0530

    path ka lafda fix karo

--- run_mongo.sh
+++ run_mongo.sh
@@ -2,7 +2,11 @@
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 data_dir_path="$parent_path/db_data/db/"
-log_path="$parent_path/db_data/out/log.txt"
+log_path="$parent_path/db_data/log.txt"
 echo "data_dir_path : $data_dir_path"
 echo "log_path : $log_path"
+
+#Make data dirs if needed. 
+mkdir -p $data_dir_path
+
 nohup mongod --dbpath $data_dir_path >> $log_path &
